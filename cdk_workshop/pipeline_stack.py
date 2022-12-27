aa22683a5295fafcd73f86ff9edc565fbe051ca4
Hassan-Islam <hassananxari4@gmail.com>
2022-12-28 04:39:14 +0500

    Add deploy stage to pipeline

--- cdk_workshop/pipeline_stack.py
+++ cdk_workshop/pipeline_stack.py
@@ -3,7 +3,7 @@ from aws_cdk import (
     Stack,
     aws_codecommit as codecommit,
 )
-
+from cdk_workshop.pipeline_stage import WorkshopPipelineStage
 class WorkshopPipelineStack(Stack):
 
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
@@ -26,3 +26,5 @@ class WorkshopPipelineStack(Stack):
                 ]
             ),
         )
+        deploy = WorkshopPipelineStage(self, "Deploy")
+        deploy_stage = pipeline.add_stage(deploy)
